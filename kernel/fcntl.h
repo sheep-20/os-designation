8cad7c70b1740a71b5c87b9231386d2cd8e7cd38
sheep-20 <2090499482@qq.com>
2025-09-04 13:20:30 +0800

    lab9-2 finished

--- kernel/fcntl.h
+++ kernel/fcntl.h
@@ -1,5 +1,12 @@
-#define O_RDONLY  0x000
-#define O_WRONLY  0x001
-#define O_RDWR    0x002
-#define O_CREATE  0x200
-#define O_TRUNC   0x400
+#define T_DIR 1      // Directory
+#define T_FILE 2     // File
+#define T_DEVICE 3   // Device
+#define T_SYMLINK 4  // Symbolic links
+
+#define O_RDONLY 0x000
+#define O_WRONLY 0x001
+#define O_RDWR 0x002
+#define O_CREATE 0x200
+#define O_TRUNC 0x400
+#define O_NOFOLLOW 0x100
+
